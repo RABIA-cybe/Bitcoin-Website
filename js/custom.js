e8d9b372e1e0816cf598679747aca4a58778d4ec
RABIA-cybe <rabia.rpo@gmail.com>
2023-08-04 18:29:45 +0500

    News section completed

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 15,
+    shadowScale: 0.94,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
